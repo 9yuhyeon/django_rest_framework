98fd5a97ebf63133ca4c5a0b9685bc18b607903a
9yuhyeon <kkh2742@gmail.com>
2022-11-12 20:21:24 +0900

    add : 회원가입, (jwt)커스텀 토큰 인증 방식 로그인 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from users import views
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('signup/', views.UserView.as_view(), name='user_view'),
+    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
